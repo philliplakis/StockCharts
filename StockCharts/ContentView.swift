78749f4a2ecb89c380cd851488dff3e700e4bc89
Phillip Lakis <p.lakis@icloud.com>
2021-01-20 12:13:54 +1100

    Initial Commit

---
+++ StockCharts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StockCharts
+//
+//  Created by Phillip Lakis on 20/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StockCharts/StockChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockChartsApp.swift
+//  StockCharts
+//
+//  Created by Phillip Lakis on 20/1/21.
+//
+
+import SwiftUI
+
+@main
+struct StockChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
